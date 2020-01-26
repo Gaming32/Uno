26c4becc2a754e6e0a856e068547d79e4fcf3d49
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-01-26 16:04:56 -0600

    a little network stuff

--- uno/_network/__init__.py
+++ uno/_network/__init__.py
@@ -1,13 +1,15 @@
+import _thread
 from time import sleep
 from .._player import Player
 from .server import GameServer
 
-class ServerHandler(Player):
+class ServerHandler:
     _player_count_so_far = 0
     _initted = False
     server = None
     card_count = None
     def __init__(self, card_count=7):
+        self.id = _player_count_so_far
         ServerHandler.card_count = card_count
         ServerHandler._player_count_so_far += 1
     def poll_loop(self):
@@ -18,10 +20,18 @@ class ServerHandler(Player):
         if not ServerHandler._initted:
             ServerHandler._initted = True
             ServerHandler.server = GameServer(None, ('', 1920), self._player_count_so_far)
+            _thread.start_new_thread(self.poll_loop, ())
     def __del__(self):
         ServerHandler._player_count_so_far = 0
         ServerHandler._initted = False
         ServerHandler.server = None
         ServerHandler.card_count = None
+    def _function(self, funcname, args=[], kwargs={}):
+        self.server.Send({'action': funcname, 'args': args, 'kwargs': kwargs})
+
+    def start(self):
+        pass
+
+def _client_poll_loop(ql, listener): ...
 
 def connect_to_server(): ...
--- uno/_network/server.py
+++ uno/_network/server.py
@@ -3,5 +3,11 @@ from .channel import GameChannel
 
 class GameServer(Server):
     channelClass = GameChannel
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.conn_count = 0
+        self.channels = {}
+    def Connected(self, channel, addr):
+        self.channels[self.conn_count] = channel
+        channel.Send({'action': '_init', 'id': self.conn_count})
+        self.conn_count += 1
